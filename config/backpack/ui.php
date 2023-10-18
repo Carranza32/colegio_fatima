55b494bc9a679ca6e7fefa37d22c934e20a713a3
Mario Carranza <mario.carranza996@gmail.com>
2023-10-18 06:37:27 -0600

    update

--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -76,7 +76,7 @@ return [
     'developer_name' => '',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => '',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => false,
